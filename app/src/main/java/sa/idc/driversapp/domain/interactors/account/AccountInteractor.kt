4c3f53e6fd0f7184a4d15bf8e21e9b8c1fbb4689
Timur <minteemer@gmail.com>
2018-10-10 17:10:41 +0300

    Login interactor finished

--- app/src/main/java/sa/idc/driversapp/domain/interactors/account/AccountInteractor.kt
+++ app/src/main/java/sa/idc/driversapp/domain/interactors/account/AccountInteractor.kt
@@ -1,6 +1,7 @@
 package sa.idc.driversapp.domain.interactors.account
 
 import sa.idc.driversapp.repositories.account.DummyAccountRepositoryImpl
+import sa.idc.driversapp.repositories.preferences.AppPreferences
 
 class AccountInteractor {
 
@@ -8,7 +9,16 @@ class AccountInteractor {
         Success, WrongPassword, ConnectionError
     }
 
+    private val preferences = AppPreferences.instance
     private val repository: AccountRepository = DummyAccountRepositoryImpl()
 
-    fun login(login: String, password: String) = repository.login(login, password)
+    fun login(login: String, password: String) = repository.login(login, password).map {
+        val (loginResult, token) = it
+        if (loginResult == LoginResult.Success){
+            preferences.login = login
+            preferences.token = token
+        }
+
+        loginResult
+    }
 }
