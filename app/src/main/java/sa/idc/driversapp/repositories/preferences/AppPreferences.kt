3bfe2e616db17cb187f2da0ae8c01332bc108a0d
Timur <minteemer@gmail.com>
2018-10-10 16:12:35 +0300

    Application preferences added

---
+++ app/src/main/java/sa/idc/driversapp/repositories/preferences/AppPreferences.kt
@@ -0,0 +1,30 @@
+package sa.idc.driversapp.repositories.preferences
+
+import android.content.Context
+import sa.idc.driversapp.IDCDriversApp
+
+class AppPreferences private constructor(context: Context) :
+        BaseSharedPreferences(context.getSharedPreferences(NAME, Context.MODE_PRIVATE)) {
+
+    companion object {
+        const val NAME = "app_preferences"
+
+        val instance: AppPreferences by lazy { AppPreferences(IDCDriversApp.instance) }
+    }
+
+
+    var token: String by stringPreference(Keys.TOKEN, DefaultValues.TOKEN)
+
+    var login: String by stringPreference(Keys.LOGIN, DefaultValues.LOGIN)
+
+
+    object DefaultValues {
+        const val TOKEN = ""
+        const val LOGIN = ""
+    }
+
+    private object Keys {
+        const val TOKEN = "token"
+        const val LOGIN = "login"
+    }
+}
