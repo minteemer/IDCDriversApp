eb18a078546eb3acec97567dfc3cbacdb8967721
arsee2 <kuzmin.arsee@yandex.ru>
2018-11-01 00:41:52 +0300

    Image is centralised

--- app/src/main/java/sa/idc/driversapp/repositories/driverTasks/DriverTasksRepositoryImpl.kt
+++ app/src/main/java/sa/idc/driversapp/repositories/driverTasks/DriverTasksRepositoryImpl.kt
@@ -15,7 +15,9 @@ class DriverTasksRepositoryImpl : DriverTasksRepository {
 
     private val db = DBHelper.defaultStorIOBuilder.build()
 
-    override fun refreshTasks(): Single<List<DriverTask>> =
+    override fun refreshTasks(): Single<List<DriverTask>> = Single.just(listOf())
+
+    fun hui() =
             tasksApi.getTasksList().map { response ->
                 response.result?.map { it.toDomainEntity() } ?: throw response.resultError()
             }.flatMap { tasks ->
